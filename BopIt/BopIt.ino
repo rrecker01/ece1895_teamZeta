4a796cc3e699d6cbf7ec3889a4a127453b41e678
rrecker01 <ryanrecker1@gmail.com>
2023-03-25 11:16:07 -0400

    Arcade Bop-It! v1.0

--- BopIt/BopIt.ino
+++ BopIt/BopIt.ino
@@ -15,22 +15,18 @@ const int DDR = 6;
 const int PB = 7;
 const int stick = 8;
 
-const int coin = 4;     //Coin slot connected to D2
-const int pinball = 6;  //Pinball plunger connected to D4
-const int DDRLeft = 11; //DDR Button Left connected to D5
-const int DDRRight = 12;//DDR Button Right connected to D6
-const int DDRUp = 13;   //DDR Button Up connected to D7
-const int DDRDown = 14; //DDR Button Down connected to D8
-const int DDRBlue = 18; //Blue DDR Buttons connected to D12
-const int DDRRed = 19;  //Red DDR Buttons connected to D13
-const int JSx = 23;     //Joystick x-axis connected to A0
-const int JSy = 24;     //Joystick y-axis connected to A1
+const int coin = 6;     //Coin slot connected to D2
+const int pinball = 2;  //Pinball plunger connected to D4
+const int DDRLeft = 9; //DDR Button Left connected to D5
+const int DDRUp = 11;   //DDR Button Up connected to D7
+const int JSx = A0;     //Joystick x-axis connected to A0
+const int JSy = A1;     //Joystick y-axis connected to A1
 
 
 int prevIn = 0;         //store last input
 int gameMap[99];        //array of inputs the game wants
 int turn = 0;           //round the player is on
-int inDelay = 650;     //delay between inputs, reset at the start of every game
+int inDelay = 1000;      //delay between inputs, reset at the start of every game
 bool start = false;     //game won't start until start is true
 
 void setup() {
@@ -46,78 +42,90 @@ void setup() {
   pinMode(coin, INPUT);
   pinMode(pinball, INPUT);
   pinMode(DDRLeft, INPUT);
-  pinMode(DDRRight, INPUT);
   pinMode(DDRUp, INPUT);
-  pinMode(DDRDown, INPUT);
-  pinMode(DDRBlue, OUTPUT);
-  pinMode(DDRRed, OUTPUT);
   pinMode(JSx, INPUT);
-  pinMode(JSy, INPUT);
 }
 
 void loop() {
   //pregame behavior
+  prevIn = 0;
   lcd.clear();
-  if(start == false){
+  while(start == false){
     prevIn = 0;
     turn = 0;
-    inDelay = 1000;
-    randomSeed(analogRead(0));
+    inDelay = 1200;
+    randomSeed(analogRead(A0));
     for(int i = 0 ; i < 99 ; i++){
-      gameMap[i] = random(1,4);
+      gameMap[i] = random(1,5);
     }
     lcd.setCursor(5,0);
     lcd.print("INSERT");
     lcd.setCursor(6,1);
     lcd.print("COIN");
+    //start?
+    if(digitalRead(coin) == HIGH){
+      start = true;
+      player.play(1);
+    }
   }
 
-  //start?
-  if(digitalRead(coin) == HIGH){
-    start = true;
-    player.play(1);
-  }
+  
 
   while(start == true){
     lcd.clear();
     if(gameMap[turn] == 1){
         player.play(8);
         lcd.print("WIGGLE!");
-      }else if(gameMap[turn] == 2){
-        digitalWrite(DDRBlue, HIGH);
-        player.play(6);
-        lcd.print("DANCE!");
       }else if(gameMap[turn] == 3){
-        digitalWrite(DDRRed, HIGH);
         player.play(6);
-        lcd.print("DANCE!");
+        lcd.print("FORWARD!");
+      }else if(gameMap[turn] == 2){
+        player.play(6);
+        lcd.print("LEFT!");
       }else if(gameMap[turn] == 4){
         player.play(7);
         lcd.print("PINBALL!");
     }
+
     for(int i = 0; i < inDelay; i++){
-      if(gameMap[turn] == 1){
-        prevIn = JSWiggled()*1;
-      }else if(gameMap[turn] == 2){
-        prevIn = (buttonBlue()||buttonRed())*2;
-      }else if(gameMap[turn] == 3){
-        prevIn = (buttonRed()||buttonBlue())*3;
-      }else if(gameMap[turn] == 4){
-        prevIn = pinIn()*4;
+      lcd.setCursor(0,1);
+      if(analogRead(JSx) < 450 || analogRead(JSx) > 550){
+        prevIn = 1;
       }
+      else if(digitalRead(DDRLeft) == HIGH){
+        prevIn = 2;
+      }
+      else if(digitalRead(DDRUp) == HIGH){
+        prevIn = 3;
+      } 
+      else if(pinIn()){
+        prevIn = 4;
+      }
+      // lcd.setCursor(12,0);
+      // lcd.print(analogRead(JSx));
+      // lcd.setCursor(0,1);
+      // lcd.print(prevIn);
       delay(1);
     }
 
     if(prevIn == gameMap[turn]){
-      prevIn = 0;
       if(turn%10 == 0){
         inDelay -= 50;
         player.play(5);
-      }   
+      }
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print(gameMap[turn]);
+      lcd.setCursor(0,1);
+      lcd.print(prevIn);
+      lcd.setCursor(4,0);
+      lcd.print("CORRECT");   
+      prevIn = 0;  
       turn += 1;
-      delay(1000);   
+      delay(500); 
     }else{
       lcd.clear();
+      delay(500);
       lcd.setCursor(6,0);
       lcd.print("GAME");
       lcd.setCursor(6,1);
@@ -126,15 +134,13 @@ void loop() {
       lcd.clear();
       lcd.setCursor(4, 0);
       lcd.print("HI SCORE");
-      lcd.setCursor(7,0);
       for(int i = 0; i <= turn; i++){
-        lcd.clear();
-        if(i%10 == 0)
-          player.play(4);
-        lcd.write(turn);          
+        lcd.setCursor(7,1);
+        lcd.print(i);
+        delay(100);
       }
-      delay(1000);
-      start = 0;
+      delay(3000);
+      start = false;
     }  
 
     //win screen
@@ -144,39 +150,37 @@ void loop() {
       lcd.print("YOU");
       lcd.setCursor(6,1);
       lcd.print("WIN");
-      delay(500);
+      delay(1000);
       lcd.clear();
       lcd.setCursor(4,0);
       lcd.print("HI SCORE");
-      lcd.setCursor(7,0);
+      lcd.setCursor(7,1);
       for(int i = 0; i <= turn; i++){
-        lcd.clear();
-        if(i%10 == 0)
-          player.play(4);
-        lcd.write(turn);          
+        lcd.setCursor(7,1);
+        lcd.print(i);
+        delay(100);
       }
-      delay(1000);
-      start = 0;
+      delay(3000);
+      start = false;
     }
   }
 }
 
 bool JSWiggled(){
-  if(analogRead(JSx < 450 || JSx > 550))
-    return true;
-  else
+  if(analogRead(JSx) < 450 || analogRead(JSx) > 550)
     return false;
+  else
+    return true;
 }
 
 bool pinIn(){
-
-  return digitalRead(pinball);
+  return (digitalRead(pinball) == HIGH);
 }
 
 bool buttonBlue(){
-  return digitalRead(DDRUp) || digitalRead(DDRDown);
+  return digitalRead(DDRLeft);
 }
 
 bool buttonRed(){
-  return digitalRead(DDRLeft) || digitalRead(DDRRight);
+  return digitalRead(DDRUp);
 }
